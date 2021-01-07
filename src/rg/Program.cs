1cd6202f86835071c433a4d4732da8c5230dac05
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-08 02:26:45 +0900

    ver 1.0.0.0

---
+++ src/rg/Program.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Copyright (C) 2021 Akitsugu Komiyama
+ * under the MIT License
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+
+
+
+internal static class StringEncodeExtension
+{
+    public static string EncodeCommandLineValue(this string value)
+    {
+        if (string.IsNullOrEmpty(value)) return "";
+        var containsSpace = value.IndexOfAny(new[] { ' ', '\t' }) != -1;
+
+        // 「\…\"」をエスケープ
+        // やってることは、「"」直前の「\」の数を 2倍+1
+        value = _commandLineEscapePattern.Replace(value, @"$1\$&");
+
+        // スペース／タブが含まれる場合はデリミタで囲み、末尾が「\」だった場合、エスケープ
+        if (containsSpace)
+        {
+            value = "\"" + _lastBackSlashPattern.Replace(value, "$1$1") + "\"";
+        }
+        return value;
+    }
+    private static readonly Regex _commandLineEscapePattern = new Regex("(\\\\*)\"");
+    private static readonly Regex _lastBackSlashPattern = new Regex(@"(\\+)$");
+
+    /// 
+    /// コマンドライン引数複数個をエンコードして、スペースで結合
+    /// 
+    public static string EncodeCommandLineValues(this IEnumerable<string> values)
+    {
+        if (values == null) throw new ArgumentNullException("values");
+        return string.Join(" ", values.Select(v => EncodeCommandLineValue(v)));
+    }
+}
+
+
+internal class RipGrepCommandLine
+{
+    static Dictionary<String, bool> hit_string_dictionary = new Dictionary<String, Boolean>();
+
+    Process process =  new Process();
+
+    List<String> arg_list = null;
+    List<String> arg_list_head_for_sjis = new List<String> { "-E", "sjis" };
+
+    const String rg_utf8_name = "rg_utf8.exe";
+
+    public RipGrepCommandLine(String[] args)
+    {
+        if (arg_list == null)
+        {
+            arg_list = new List<string>(args);
+        }
+
+        //起動するファイルを指定する
+        var self = Assembly.GetExecutingAssembly().Location;
+        process.StartInfo.FileName = Path.GetDirectoryName(self) + rg_utf8_name;
+
+    }
+
+    string MakeArgsString(Encoding enc)
+    {
+        if (enc == Encoding.GetEncoding(932))
+        {
+            arg_list.Insert(0, "sjis");
+            arg_list.Insert(0, "-E");
+        }
+
+        string arg_line = arg_list.EncodeCommandLineValues();
+        System.Diagnostics.Trace.WriteLine(arg_line);
+        return arg_line;
+    }
+
+    public void Grep(Encoding enc)
+    {
+        try
+        {
+            string arg_line = MakeArgsString(enc);
+
+            process.StartInfo.Arguments = arg_line;
+
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+
+            //イベントハンドラの追加
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardOutput = true;
+
+            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+
+            process.ErrorDataReceived += proc_ErrorDataReceived;
+            process.OutputDataReceived += proc_OutputDataReceived;
+
+
+            //起動する
+            process.Start();
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+
+            try
+            {
+                if (process != null)
+                {
+                    process.Close();
+                    process.Kill();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex.Message);
+        }
+
+    }
+
+    private void proc_OutputDataReceived(object sender, DataReceivedEventArgs ev)
+    {
+        String data = ev.Data;
+        try
+        {
+            if (data != null)
+            {
+                lock (hit_string_dictionary)
+                {
+                    // 今度は、まだ登録されていない時だけ、SJIS版を吐き出す
+                    if (!hit_string_dictionary.ContainsKey(data))
+                    {
+                        hit_string_dictionary.Add(data, true);
+                        Console.WriteLine(data);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex.Message);
+        }
+
+    }
+
+    private void proc_ErrorDataReceived(object sender, DataReceivedEventArgs ev)
+    {
+        proc_OutputDataReceived(sender, ev);
+    }
+}
+
+
+
+public class RG
+{
+    public static void Main(String[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        RipGrepCommandLine rgcl1 = new RipGrepCommandLine(args);
+        rgcl1.Grep(Encoding.UTF8);
+
+        RipGrepCommandLine rgcl2 = new RipGrepCommandLine(args);
+        rgcl2.Grep(Encoding.GetEncoding(932));
+    }
+}
---
+++ src/rg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("rg_sjis")]
+[assembly: AssemblyDescription("Visual Studio Code の Grep 検索を SJIS対応")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Visual Studio Code の Grep 検索を SJIS対応")]
+[assembly: AssemblyCopyright("Copyright © 2021 Akitsugu Komiyama")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("422ca314-5551-4fd4-8fc3-1f3757b8dd02")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
